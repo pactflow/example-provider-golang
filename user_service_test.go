9bef733a47727356d0d3c87fd6e4eb07be054c3c
Yousaf Nabi <yousaf@pactflow.io>
2025-01-27 21:41:42 +0000

    chore(test): strip test down to basic elements to try and identify cgo fault on musl

--- user_service_test.go
+++ user_service_test.go
@@ -4,57 +4,20 @@ import (
 	"fmt"
 	"os"
 	"testing"
-	"time"
-
 	"github.com/gin-gonic/gin"
-	"github.com/pact-foundation/pact-go/v2/models"
 	"github.com/pact-foundation/pact-go/v2/provider"
 	"github.com/pact-foundation/pact-go/v2/utils"
 )
-
-func parseDate(dateStr string) *time.Time {
-	date, err := time.Parse("2006-01-02", dateStr)
-	if err != nil {
-		panic(err)
-	}
-	return &date
-}
 
 func TestPactProvider(t *testing.T) {
 	go startProvider()
 
 	verifier := provider.NewVerifier()
-
-	// Verify the Provider - fetch pacts from PactFlow
 	verifyRequest := provider.VerifyRequest{
-		Provider:                   "pactflow-example-provider-golang",
-		ProviderBaseURL:            fmt.Sprintf("http://127.0.0.1:%d", port),
-		BrokerToken:                os.Getenv("PACT_BROKER_TOKEN"),
-		BrokerUsername:             os.Getenv("PACT_BROKER_USERNAME"),
-		BrokerPassword:             os.Getenv("PACT_BROKER_PASSWORD"),
-		PublishVerificationResults: envBool("PACT_BROKER_PUBLISH_VERIFICATION_RESULTS"),
-		ProviderVersion:            os.Getenv("GIT_COMMIT"),
-		StateHandlers:              stateHandlers,
-		ProviderBranch:             os.Getenv("GIT_BRANCH"),
+		Provider:        "pactflow-example-provider-golang",
+		ProviderBaseURL: fmt.Sprintf("http://127.0.0.1:%d", port),
 	}
-
-	if os.Getenv("PACT_FILE") != "" {
-		// Allow user to pass a local file for verification
-		verifyRequest.PactFiles = []string{os.Getenv("PACT_FILE")}
-	} else if os.Getenv("PACT_URL") != "" {
-		// For builds triggered by a 'contract_requiring_verification_published' webhook, verify the changed pact against latest of mainBranch and any version currently deployed to an environment
-		// https://docs.pact.io/pact_broker/webhooks#using-webhooks-with-the-contract_requiring_verification_published-event
-		// The URL will have been passed in from the webhook to the CI job.
-		verifyRequest.PactURLs = []string{os.Getenv("PACT_URL")}
-	} else {
-		// For 'normal' provider builds, fetch the the latest version from the main branch of each consumer, as specified by
-		// the consumer's mainBranch property and all the currently deployed and currently released and supported versions of each consumer.
-		// https://docs.pact.io/pact_broker/advanced_topics/consumer_version_selectors
-		verifyRequest.BrokerURL = fmt.Sprint(os.Getenv("PACT_BROKER_BASE_URL"))
-		verifyRequest.ConsumerVersionSelectors = getSelectors()
-		verifyRequest.EnablePending = true
-		verifyRequest.IncludeWIPPactsSince = parseDate("2024-01-01")
-	}
+	verifyRequest.PactFiles = []string{os.Getenv("PACT_FILE")}
 
 	err := verifier.VerifyProvider(t, verifyRequest)
 	if err != nil {
@@ -63,59 +26,12 @@ func TestPactProvider(t *testing.T) {
 
 }
 
-// Provider state handlers
-var stateHandlers = models.StateHandlers{
-	"a product with ID 10 exists": func(setup bool, s models.ProviderState) (models.ProviderStateResponse, error) {
-		productRepository = productExists
-		return models.ProviderStateResponse{}, nil
-	},
-	"no products exist": func(setup bool, s models.ProviderState) (models.ProviderStateResponse, error) {
-		productRepository = noProductsExist
-		return models.ProviderStateResponse{}, nil
-	},
-}
-
-// Starts the provider API with hooks for provider states.
 func startProvider() {
 	router := gin.Default()
 	router.GET("/product/:id", GetProduct)
-
 	router.Run(fmt.Sprintf(":%d", port))
 }
 
-// Provider States data sets
-var productExists = &ProductRepository{
-	Products: map[string]*Product{
-		"10": {
-			Name:    "Pizza",
-			ID:      "10",
-			Type:    "food",
-			Version: "1.0.0",
-		},
-	},
-}
-
-var noProductsExist = &ProductRepository{}
 
 // Configuration / Test Data
 var port, _ = utils.GetFreePort()
-
-func getSelectors() []provider.Selector {
-	selectors := make([]provider.Selector, 0)
-	if os.Getenv("SELECTORS") != "" {
-		selectors = []provider.Selector{
-			&provider.ConsumerVersionSelector{
-				DeployedOrReleased: true,
-			},
-			&provider.ConsumerVersionSelector{
-				MainBranch: true,
-			},
-		}
-	}
-
-	return selectors
-}
-
-func envBool(k string) bool {
-	return os.Getenv(k) != ""
-}
